acfe900f68c3226b8b9f4e616eaea1826d8689c9
Masha Cernivec <masa179@gmail.com>
2018-01-05 17:42:25 +0000

    First weather app implementation

---
+++ src/actions/index.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API_KEY = "1234";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+export const FETCH_WEATHER_ERROR = "FETCH_WEATHER_ERROR";
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},uk`;
+  const request = axios.get(url).then(response => (
+    {
+      type: FETCH_WEATHER,
+      payload: response
+    }
+  )).catch(() => (
+    {
+      type: FETCH_WEATHER_ERROR
+    }
+  ));
+
+  return request;
+}
---
+++ src/containers/weather_list.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Chart from '../components/chart';
+
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    if (!cityData) {
+      return <p>Oh no</p>
+    }
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+
+    return(
+      <tr key={name}>
+        <td>{name}</td>
+        <td><Chart color="green" data={temps} units="C" /></td>
+        <td><Chart color="blue" data={pressures} units="hPA" /></td>
+        <td><Chart color="purple" data={humidities} units="%" /></td>
+      </tr>
+    )
+  }
+
+  render() {
+    return(
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature (C)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
+
